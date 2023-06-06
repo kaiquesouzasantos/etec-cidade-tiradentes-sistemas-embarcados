7ca55d3fea31a4aec4ab166a2dc6593edc01fb45
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-06-06 05:34:11 -0300

    Update botao_controlador_lampada.cpp

--- exercicios/botao_controlador_lampada/botao_controlador_lampada.cpp
+++ exercicios/botao_controlador_lampada/botao_controlador_lampada.cpp
@@ -1,4 +1,4 @@
-int CONT_LAMPADA = 0, botao = 2, lampada = 4;
+int botao = 2, lampada = 4;
 
 void setup() {
     pinMode(botao, INPUT); // BOTAO
@@ -7,12 +7,10 @@ void setup() {
 
 void loop() {  
     if (digitalRead(botao) == 1) {
-        if (CONT_LAMPADA == 0){
+        if (digitalRead(lampada) == 0){
     	    digitalWrite(lampada, HIGH);
-      	    CONT_LAMPADA = 1;
-        } else if (CONT_LAMPADA == 1) {
+        } else {
       	    digitalWrite(lampada, LOW);
-      	    CONT_LAMPADA = 0;
         }
     }
 }
